96bbfd93142fae29bfde38007ed9fe0d6831a736
Jhon-Salguero <jhonedisson.salguero221@comunidadunir.net>
2025-03-01 13:07:50 -0500

    cambios

--- src/views/BookDetails.js
+++ src/views/BookDetails.js
@@ -13,7 +13,7 @@ const BookDetails = () => {
   useEffect(() => {
     const fetchBookDetails = async () => {
       try {
-        const response = await fetch("https://d65d-186-154-211-109.ngrok-free.app/ms-books-catalogue/api/books", {
+        const response = await fetch("https://ed7a-191-107-208-118.ngrok-free.app/ms-books-catalogue/api/books", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
--- src/views/BooksList.js
+++ src/views/BooksList.js
@@ -18,7 +18,7 @@ const BooksList = () => {
   const { addToCart } = useCart();
 
   // URL de la API Gateway
-  const API_URL = "http://localhost:8762/ms-books-catalogue/api/books";
+  const API_URL = "https://ed7a-191-107-208-118.ngrok-free.app/ms-books-catalogue/api/books";
 
   // Cargar los libros desde la API con filtro visible=true
   useEffect(() => {
